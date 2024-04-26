51cd8d4f41c2bfd3d5dcaef42b48594b218943e4
Jules Fouchy <jules.fouchy@ntymail.com>
2024-04-26 13:19:54 +0200

    🧼 Cleanup

--- src/VideoDecoder.cpp
+++ src/VideoDecoder.cpp
@@ -58,57 +58,12 @@ AVFrame* VideoDecoder::convertFrameToRGBA(AVFrame* frame, AVFrame* rgbaFrame) co
     rgbaFrame->format = frame->format;
 }
 
-int VideoDecoder::output_video_frame(AVFrame* frame)
-{
-    if (frame->width != width || frame->height != height || frame->format != pix_fmt)
-    {
-        /* To handle this change, one could call av_image_alloc again and
-         * decode the following frames into another rawvideo file. */
-        fprintf(stderr,
-                "Error: Width, height and pixel format have to be "
-                "constant in a rawvideo file, but the width, height or "
-                "pixel format of the input video changed:\n"
-                "old: width = %d, height = %d, format = %s\n"
-                "new: width = %d, height = %d, format = %s\n",
-                width, height, av_get_pix_fmt_name(pix_fmt), frame->width, frame->height, av_get_pix_fmt_name(static_cast<AVPixelFormat>(frame->format)));
-        return -1;
-    }
-
-    printf("video_frame n:%d\n", video_frame_count++);
-
-    /* copy decoded frame to destination buffer:
-     * this is required since rawvideo expects non aligned data */
-    av_image_copy2(video_dst_data, video_dst_linesize, frame->data, frame->linesize, pix_fmt, width, height);
-
-    /* write to rawvideo file */
-    fwrite(video_dst_data[0], 1, video_dst_bufsize, video_dst_file);
-    return 0;
-}
-
-int VideoDecoder::output_audio_frame(AVFrame* frame)
-{
-    size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(static_cast<AVSampleFormat>(frame->format));
-    // printf("audio_frame n:%d nb_samples:%d pts:%s\n", audio_frame_count++, frame->nb_samples, av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
-
-    /* Write the raw audio data samples of the first plane. This works
-     * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
-     * most audio decoders output planar audio, which uses a separate
-     * plane of audio samples for each channel (e.g. AV_SAMPLE_FMT_S16P).
-     * In other words, this code will write only the first audio channel
-     * in these cases.
-     * You should use libswresample or libavfilter to convert the frame
-     * to packed data. */
-    fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
-
-    return 0;
-}
-
-int VideoDecoder::decode_packet(AVCodecContext* dec, const AVPacket* pkt)
+int VideoDecoder::decode_packet()
 {
     int ret = 0;
 
     // submit the packet to the decoder
-    ret = avcodec_send_packet(dec, pkt);
+    ret = avcodec_send_packet(_decoder_ctx, _packet);
     if (ret < 0)
     {
         // std::cerr << "Error submitting a packet for decoding " << av_err2str(ret) << '\n'; // TODO
@@ -118,7 +73,7 @@ int VideoDecoder::decode_packet(AVCodecContext* dec, const AVPacket* pkt)
     // get all the available frames from the decoder
     if (ret >= 0)
     {
-        ret = avcodec_receive_frame(dec, frame);
+        ret = avcodec_receive_frame(_decoder_ctx, frame);
         if (ret < 0)
         {
             // those two return values are special and mean there is no output
@@ -130,12 +85,6 @@ int VideoDecoder::decode_packet(AVCodecContext* dec, const AVPacket* pkt)
             return ret;
         }
 
-        // write the frame data to output file
-        // if (dec->codec->type == AVMEDIA_TYPE_VIDEO)
-        //     ret = output_video_frame(frame);
-        // else
-        //     ret = output_audio_frame(frame);
-
         // av_frame_unref(frame);
         if (ret < 0)
             return ret;
@@ -144,22 +93,22 @@ int VideoDecoder::decode_packet(AVCodecContext* dec, const AVPacket* pkt)
     return 0;
 }
 
-int VideoDecoder::open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVFormatContext* fmt_ctx, AVMediaType type)
+int VideoDecoder::open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVMediaType type)
 {
     int            ret, stream_index;
     AVStream*      st;
     const AVCodec* dec = NULL;
 
-    ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
+    ret = av_find_best_stream(_format_ctx, type, -1, -1, NULL, 0);
     if (ret < 0)
     {
-        fprintf(stderr, "Could not find %s stream in input file '%s'\n", av_get_media_type_string(type), src_filename);
+        fprintf(stderr, "Could not find %s stream\n", av_get_media_type_string(type));
         return ret;
     }
     else
     {
         stream_index = ret;
-        st           = fmt_ctx->streams[stream_index];
+        st           = _format_ctx->streams[stream_index];
 
         /* find decoder for the stream */
         dec = avcodec_find_decoder(st->codecpar->codec_id);
@@ -199,44 +148,28 @@ int VideoDecoder::open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, 
 VideoDecoder::VideoDecoder(std::filesystem::path const& path)
 {
     /* open input file, and allocate format context */
-    if (avformat_open_input(&fmt_ctx, path.string().c_str(), NULL, NULL) < 0)
+    if (avformat_open_input(&_format_ctx, path.string().c_str(), NULL, NULL) < 0)
     {
-        fprintf(stderr, "Could not open source file %s\n", src_filename);
+        fprintf(stderr, "Could not open source file %s\n", path.string().c_str());
         exit(1);
     }
     /* retrieve stream information */
-    if (avformat_find_stream_info(fmt_ctx, NULL) < 0)
+    if (avformat_find_stream_info(_format_ctx, NULL) < 0)
     {
         fprintf(stderr, "Could not find stream information\n");
         exit(1);
     }
 
     int ret;
-    if (open_codec_context(&video_stream_idx, &video_dec_ctx, fmt_ctx, AVMEDIA_TYPE_VIDEO) >= 0)
+    if (open_codec_context(&video_stream_idx, &_decoder_ctx, AVMEDIA_TYPE_VIDEO) >= 0)
     {
-        video_stream = fmt_ctx->streams[video_stream_idx];
+        video_stream = _format_ctx->streams[video_stream_idx];
 
         /* allocate image where the decoded image will be put */
-        width   = video_dec_ctx->width;
-        height  = video_dec_ctx->height;
-        pix_fmt = video_dec_ctx->pix_fmt; // TODO check that we support the pixel format. Or ask ffmpeg to convert to srgb?
-        ret     = av_image_alloc(video_dst_data, video_dst_linesize, width, height, pix_fmt, 1);
-        if (ret < 0)
-        {
-            fprintf(stderr, "Could not allocate raw video buffer\n");
-            // goto end;
-        }
-        video_dst_bufsize = ret;
+        width   = _decoder_ctx->width;
+        height  = _decoder_ctx->height;
+        pix_fmt = _decoder_ctx->pix_fmt; // TODO check that we support the pixel format. Or ask ffmpeg to convert to srgb?
     }
-
-    // TODO import audio too ?
-    // if (open_codec_context(&audio_stream_idx, &audio_dec_ctx, fmt_ctx, AVMEDIA_TYPE_AUDIO) >= 0)
-    // {
-    //     audio_stream   = fmt_ctx->streams[audio_stream_idx];
-    // }
-
-    /* dump input information to stderr */
-    // av_dump_format(fmt_ctx, 0, src_filename, 0);
 
     if (/* !audio_stream &&  */ !video_stream)
     {
@@ -254,8 +187,8 @@ VideoDecoder::VideoDecoder(std::filesystem::path const& path)
         // goto end;
     }
 
-    pkt = av_packet_alloc();
-    if (!pkt)
+    _packet = av_packet_alloc();
+    if (!_packet)
     {
         fprintf(stderr, "Could not allocate packet\n");
         ret = AVERROR(ENOMEM);
@@ -271,23 +204,23 @@ void VideoDecoder::move_to_next_frame()
     bool found = false;
     while (!found)
     {
-        if (av_read_frame(fmt_ctx, pkt) < 0)
+        if (av_read_frame(_format_ctx, _packet) < 0)
         {
-            av_packet_unref(pkt);
+            av_packet_unref(_packet);
             break;
         }
 
         // check if the packet belongs to a stream we are interested in, otherwise
         // skip it
         // TODO what does it mean ? Should we then try to read the frame after that one ? (NB: I think so, since a packet will only be video OR audio, every other packet is probably an audio packet)
-        if (pkt->stream_index == video_stream_idx)
+        if (_packet->stream_index == video_stream_idx)
         {
-            ret   = decode_packet(video_dec_ctx, pkt);
+            ret   = decode_packet();
             found = true;
         }
-        // else if (pkt->stream_index == audio_stream_idx)
-        //     ret = decode_packet(audio_dec_ctx, pkt);
-        av_packet_unref(pkt);
+        // else if (_packet->stream_index == audio_stream_idx)
+        //     ret = decode_packet(audio_dec_ctx, _packet);
+        av_packet_unref(_packet);
         // if (ret < 0) // TODO File end, handle this
         //     break;
     }
@@ -302,18 +235,13 @@ auto VideoDecoder::current_frame() const -> AVFrame const&
 
 VideoDecoder::~VideoDecoder()
 {
-    /* flush the decoders */
-    if (video_dec_ctx)
-        decode_packet(video_dec_ctx, NULL);
-    if (audio_dec_ctx)
-        decode_packet(audio_dec_ctx, NULL);
-    avcodec_free_context(&video_dec_ctx);
-    avcodec_free_context(&audio_dec_ctx);
-    avformat_close_input(&fmt_ctx);
-    av_packet_free(&pkt);
+    if (_decoder_ctx)
+        avcodec_send_packet(_decoder_ctx, nullptr); // flush the decoder
+    avcodec_free_context(&_decoder_ctx);
+    avformat_close_input(&_format_ctx);
+    av_packet_free(&_packet);
     av_frame_free(&frame);
     av_frame_free(&rgba_frame);
-    av_free(video_dst_data[0]);
 }
 
 } // namespace ffmpeg
--- src/VideoDecoder.hpp
+++ src/VideoDecoder.hpp
@@ -18,41 +18,38 @@ class VideoDecoder {
 public:
     explicit VideoDecoder(std::filesystem::path const& path);
     ~VideoDecoder();
+    VideoDecoder(VideoDecoder const&)                    = delete;
+    auto operator=(VideoDecoder const&) -> VideoDecoder& = delete;
+    VideoDecoder(VideoDecoder&&) noexcept;                    // TODO
+    auto operator=(VideoDecoder&&) noexcept -> VideoDecoder&; // TODO
 
     void move_to_next_frame();
-    auto current_frame() const -> AVFrame const&;
-
+    auto current_frame() const -> AVFrame const&; // TODO take a desired format as param // TODO return our own Frame type, that only contains info we now are valid (like width and height that we copy from the other frame)
 private:
-    int      output_video_frame(AVFrame* frame);
-    int      output_audio_frame(AVFrame* frame);
-    int      decode_packet(AVCodecContext* dec, const AVPacket* pkt);
-    int      open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVFormatContext* fmt_ctx, AVMediaType type);
+    int      decode_packet();
+    int      open_codec_context(int* stream_idx, AVCodecContext** dec_ctx, AVMediaType type);
     AVFrame* convertFrameToRGBA(AVFrame* frame, AVFrame* rgbaFrame) const;
 
 private:
-    AVFormatContext* fmt_ctx{};
+    // TODO use pimpl to store all of these ? A unique_ptr will make sure we never have a bug where we forgot to ass one member to the move constructor. And we will never crretae 1000s of videos, so the cost is negligeable
+    // and also allows to easily cleanup during constructor
 
-    AVCodecContext *video_dec_ctx = NULL, *audio_dec_ctx;
-    int             width, height;
-    AVPixelFormat   pix_fmt;
-    AVStream *      video_stream = NULL, *audio_stream = NULL;
-    const char*     src_filename       = NULL;
-    const char*     video_dst_filename = NULL;
-    const char*     audio_dst_filename = NULL;
-    FILE*           video_dst_file     = NULL;
-    FILE*           audio_dst_file     = NULL;
+    // Contexts
+    AVFormatContext* _format_ctx{};
+    AVCodecContext*  _decoder_ctx{};
 
-    uint8_t* video_dst_data[4] = {NULL};
-    int      video_dst_linesize[4];
-    int      video_dst_bufsize;
-
-    int              video_stream_idx = -1, audio_stream_idx = -1;
+    // Data
     AVFrame*         frame      = NULL;
     mutable AVFrame* rgba_frame = NULL;
     mutable uint8_t* rgbaBuffer{};
-    AVPacket*        pkt               = NULL;
-    int              video_frame_count = 0;
-    int              audio_frame_count = 0;
+    AVPacket*        _packet{};
+
+    // Info
+    int           width{};
+    int           height{};
+    AVPixelFormat pix_fmt;
+    AVStream*     video_stream{};
+    int           video_stream_idx{};
 };
 
 } // namespace ffmpeg
