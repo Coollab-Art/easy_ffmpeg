2efc399de177a999dd816a86dabcc9b1b6774952
Jules Fouchy <jules.fouchy@ntymail.com>
2024-04-30 01:21:15 +0200

    ⏰ Benchmarking setup

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,10 +3,12 @@
 #include <glfw/include/GLFW/glfw3.h>
 #include <imgui.h>
 #include <libavutil/frame.h>
+#include <chrono>
 #include <cstdint>
 #include <easy_ffmpeg/easy_ffmpeg.hpp>
 #include <exception>
 #include <fstream>
+#include <numeric>
 #include <quick_imgui/quick_imgui.hpp>
 #include <stdexcept>
 #include "exe_path/exe_path.h"
@@ -61,6 +63,34 @@ auto make_texture() -> GLuint
     return textureID;
 }
 
+class AverageTime {
+public:
+    void start()
+    {
+        _start_time = std::chrono::steady_clock::now();
+    }
+    void end()
+    {
+        auto const end_time = std::chrono::steady_clock::now();
+        _times.push_back(static_cast<float>((end_time - _start_time).count()) / 1'000'000.f);
+        if (_times.size() > 300)
+            _times.erase(_times.begin());
+    }
+    auto average_time_ms() const -> float
+    {
+        return std::accumulate(_times.begin(), _times.end(), 0.f) / _times.size();
+    }
+
+    void imgui_plot()
+    {
+        ImGui::PlotLines("Timings (ms)", _times.data(), static_cast<int>(_times.size()));
+    }
+
+private:
+    std::chrono::steady_clock::time_point _start_time{};
+    std::vector<float>                    _times{};
+};
+
 auto main(int argc, char* argv[]) -> int
 {
     // av_log_set_level(AV_LOG_VERBOSE);
@@ -75,43 +105,59 @@ auto main(int argc, char* argv[]) -> int
             try
             {
                 // A VideoDecoder is not allowed to be copied nor moved, so if you need those operations you need to heap-allocate the VideoDecoder and move the pointer. You should typically use std::unique_ptr for that.
-                auto   decoder = std::make_unique<ffmpeg::VideoDecoder>(exe_path::dir() / "test.gif");
+                // auto decoder = std::make_unique<ffmpeg::VideoDecoder>(exe_path::dir() / "test.gif");
+                // auto   decoder = std::make_unique<ffmpeg::VideoDecoder>("C:/Users/fouch/Downloads/LGM 2019 – Flowers and samples — an audio reactive self exploration.mp4");
+                auto   decoder = std::make_unique<ffmpeg::VideoDecoder>("C:/Users/fouch/Downloads/Moteur-de-jeu-avec-sous-titres.mp4");
                 GLuint texture_id;
 
+                AverageTime           timer{};
+                std::optional<double> time_when_paused{};
+                ffmpeg::Frame         frame{};
                 quick_imgui::loop("easy_ffmpeg tests", [&]() {
-                    static bool first{true};
-                    if (first)
+                    if (!time_when_paused.has_value())
                     {
-                        first      = false;
-                        texture_id = make_texture();
-                        glfwSwapInterval(0);
-                    }
-                    ffmpeg::Frame frame = decoder->get_frame_at(glfwGetTime(), ffmpeg::SeekMode::Exact);
-                    if (frame.is_last_frame)
-                        glfwSetTime(0.); // Next frame we will start over at the beginning of the file
+                        static bool first{true};
+                        if (first)
+                        {
+                            first      = false;
+                            texture_id = make_texture();
+                            // glfwSwapInterval(0);
+                        }
+                        timer.start();
+                        frame = decoder->get_frame_at(glfwGetTime(), ffmpeg::SeekMode::Exact);
+                        timer.end();
+                        if (frame.is_last_frame) // TODO when not doing anything when we reach last frame, there is a jump after freezing for a bit
+                            glfwSetTime(0.);     // Next frame we will start over at the beginning of the file
 
-                    if (frame.is_different_from_previous_frame) // Optimisation: don't recreate the texture unless the frame has actually changed
-                    {
-                        glBindTexture(GL_TEXTURE_2D, texture_id);
-                        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame.width, frame.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame.data);
+                        if (frame.is_different_from_previous_frame) // Optimisation: don't recreate the texture unless the frame has actually changed
+                        {
+                            glBindTexture(GL_TEXTURE_2D, texture_id);
+                            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame.width, frame.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame.data);
+                        }
                     }
-                    // decoder.set_time(glfwGetTime());
-                    // decoder.move_to_next_frame();
-                    // glDeleteTextures(1, &texture_id);
-                    // auto const& frame = decoder.current_frame();
-                    // texture_id        = make_texture(frame);
 
                     ImGui::Begin("easy_ffmpeg tests");
-                    ImGui::Text("%.2f ms", 1000.f / ImGui::GetIO().Framerate);
+                    ImGui::Text("%.2f ms", timer.average_time_ms());
                     ImGui::Text("Time: %.2f", glfwGetTime());
                     if (ImGui::Button("-10s"))
                         glfwSetTime(glfwGetTime() - 10.);
                     ImGui::SameLine();
                     if (ImGui::Button("+10s"))
                         glfwSetTime(glfwGetTime() + 10.);
+                    timer.imgui_plot();
                     ImGui::Image(static_cast<ImTextureID>(reinterpret_cast<void*>(static_cast<uint64_t>(texture_id))), ImVec2{900.f * static_cast<float>(frame.width) / static_cast<float>(frame.height), 900.f});
                     ImGui::End();
                     ImGui::ShowDemoWindow();
+
+                    if (time_when_paused.has_value())
+                        glfwSetTime(*time_when_paused);
+                    if (ImGui::IsKeyPressed(ImGuiKey_Space))
+                    {
+                        if (time_when_paused.has_value())
+                            time_when_paused.reset();
+                        else
+                            time_when_paused = glfwGetTime();
+                    }
                 });
             }
             catch (std::exception const& e)
