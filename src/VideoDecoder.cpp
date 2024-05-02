7ba811142eefb7eb8659a159a7ebb3fd4f6b8102
Jules Fouchy <jules.fouchy@ntymail.com>
2024-05-02 15:27:23 +0200

    🧪 Try fix Linux

--- src/VideoDecoder.cpp
+++ src/VideoDecoder.cpp
@@ -57,8 +57,9 @@ auto VideoDecoder::retrieve_detailed_info() const -> std::string
         std::cout << "a2\n";
         auto const length = static_cast<size_t>(vsnprintf(nullptr, 0, fmt, vl));
         std::cout << "a3\n";
-        std::vector<char> buffer(length + 1);
+        std::vector<char> buffer(length + 10);
         std::cout << "a4\n";
+        std::cout << buffer.size() << '\n';
         vsnprintf(buffer.data(), length + 1, fmt, vl); // NOLINT(cert-err33-c)
         std::cout << "a5\n";
         tmp_string_for_detailed_info() += std::string{buffer.data()};
